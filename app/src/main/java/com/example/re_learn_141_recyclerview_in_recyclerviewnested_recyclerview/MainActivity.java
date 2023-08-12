29901beeddb629c1d3c06e39541df39bf507a334
NightOwl990 <chinh999ccc@gmail.com>
2023-08-12 16:20:01 +0700

    first

--- app/src/main/java/com/example/re_learn_141_recyclerview_in_recyclerviewnested_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/re_learn_141_recyclerview_in_recyclerviewnested_recyclerview/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "World", Toast.LENGTH_SHORT).show();
     }
 }
